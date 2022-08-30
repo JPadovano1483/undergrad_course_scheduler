e62bca1dc79e113bf26d71d506cda01a25b1a3fe
richard-pokrivka <rp1291@messiah.edu>
2022-08-30 09:04:26 -0400

    Updates
    Test

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './MessiahLogo.JPG';
 import './App.css';
 
 function App() {
@@ -7,15 +7,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please enter your username and password.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="App-title"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Messiah University
         </a>
       </header>
     </div>
