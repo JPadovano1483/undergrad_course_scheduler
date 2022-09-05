e92f6d53c06adab95049153943d1791c5d11964b
richard-pokrivka <rp1291@messiah.edu>
2022-09-05 19:18:08 -0400

    Login V 1.2
    Forgot User/password component

--- src/App.js
+++ src/App.js
@@ -6,15 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Please enter your username and password.
-        </p>
         <a
           className="App-title"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Messiah University
+        Please enter your username and password.        
         </a>
       </header>
     </div>
