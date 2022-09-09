f5e1235b34f01bf235026dfc12641e9be284dfc9
Jamie Padovano <jp1483@messiah.edu>
2022-09-09 15:33:25 -0400

    Revert "Merge branch 'dev'"
    This reverts commit b5c62c58bdc0601af90844c27fe55b91d91f5837, reversing
    changes made to 0d025753b5656af38342efb4f78b4aa0f86a0bfb.

--- src/App.js
+++ src/App.js
@@ -1,44 +1,23 @@
-import logo from './MessiahLogo.JPG';
+import logo from './logo.svg';
 import './App.css';
-import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
         <a
-          className="App-title"
+          className="App-link"
+          href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Please enter your username and password.
+          Learn React
         </a>
       </header>
-      <body>
-        <div class="App-Email">
-          <h1>Login</h1>
-          <form>
-            <div class="form-control">
-              <label>
-                <input type="text" required />Email
-              </label>
-            </div>
-            <div class="form-control">
-              <label id='pwd'>
-                <input type="password" required />Password
-              </label>
-            </div>
-            <Link to="/home">
-              <button type="submit" class="Login"> Login </button>
-            </Link>
-          </form>
-          <h4>
-            Forgot Username or Password? Click below to reset
-          </h4>
-          <a href="https://www.messiah.edu/">Reset</a>
-        </div>
-      </body>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<App />} />
-      <Route path='home' element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
