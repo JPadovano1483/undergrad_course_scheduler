ffa62f9345f052680a8f93a161b47c6f2580aeec
Jamie Padovano <jp1483@messiah.edu>
2022-09-19 19:17:57 -0400

    sql querying

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import logo from "./MessiahLogo.JPG"
-import "./App.css"
-import { Link } from "react-router-dom"
+import logo from './MessiahLogo.JPG';
+import './App.css';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <a className="App-title" target="_blank" rel="noopener noreferrer">
+        <a
+          className="App-title"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           Please enter your username and password.
         </a>
       </header>
@@ -17,29 +21,26 @@ function App() {
           <form>
             <div class="form-control">
               <label>
-                <input type="text" required />
-                Email
+                <input type="text" required />Email
               </label>
             </div>
             <div class="form-control">
-              <label id="pwd">
-                <input type="password" required />
-                Password
+              <label id='pwd'>
+                <input type="password" required />Password
               </label>
             </div>
             <Link to="/home">
-              <button type="submit" class="Login">
-                {" "}
-                Login{" "}
-              </button>
+              <button type="submit" class="Login"> Login </button>
             </Link>
           </form>
-          <h4>Forgot Username or Password? Click below to reset</h4>
+          <h4>
+            Forgot Username or Password? Click below to reset
+          </h4>
           <a href="https://www.messiah.edu/">Reset</a>
         </div>
       </body>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
