1a80889bcb9430c01483fa362e1dd307267d308f
Sebastian Alejos Acosta <98053292+bastialejos@users.noreply.github.com>
2022-09-22 08:51:37 -0400

    Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
   // const linkElement = screen.getByText(/Please enter your username and password./i);
   // expect(linkElement).toBeInTheDocument();
 });
