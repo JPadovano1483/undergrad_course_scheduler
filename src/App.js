8fb3803baa3e1a940e52b32d51e768871f28e2fa
andrewcoldsmith <andrewcoldsmith@gmail.com>
2022-09-22 21:35:50 -0400

    updated App.js

--- src/App.js
+++ src/App.js
@@ -16,27 +16,45 @@ function App() {
         </p>
       </header>
       <body>
+<<<<<<< HEAD
         <div class="App-Email">
           <h1>Login</h1>
           <form>
             <div class="form-control">
+=======
+        <div className="App-Email">
+          <h1>Login</h1>
+          <form>
+            <div className="form-control">
+>>>>>>> ad9e308 (updated App.js)
               <label>
                 <input type="text" required />Email
               </label>
             </div>
+<<<<<<< HEAD
             <div class="form-control">
+=======
+            <div className="form-control">
+>>>>>>> ad9e308 (updated App.js)
               <label id='pwd'>
                 <input type="password" required />Password
               </label>
             </div>
             <Link to="/home">
+<<<<<<< HEAD
               <button type="submit" class="Login"> Login </button>
+=======
+              <button type="submit" className="Login"> Login </button>
+>>>>>>> ad9e308 (updated App.js)
             </Link>
           </form>
           <h4>
             Forgot Username or Password? Click below to reset
           </h4>
+<<<<<<< HEAD
 
+=======
+>>>>>>> ad9e308 (updated App.js)
         </div>
       </body>
     </div>
