59bf86ff85adfb2ca21fc511b8147a8b3582d722
Sebastian Alejos Acosta <98053292+bastialejos@users.noreply.github.com>
2022-09-27 13:30:01 -0400

    Created admin page

--- src/Admin.js
+++ src/Admin.js
@@ -1,8 +1,39 @@
 import Navigation from "./navigation";
+import './home.css';
+import { TextField } from "@mui/material";
+import Button from '@mui/material/Button';
+import InputIcon from '@mui/icons-material/Input';
+
 
 function Admin() {
+    //Course_id
+    //course_name
+    //course_description
+    //credits
     return (
-        <Navigation />
+        <>
+            <Navigation />
+            <div className='contentContainer'>
+                <h1>Please enter course information</h1>
+                <p style={{ color: 'red' }}>Fields marked with * are required</p>
+                <div className="inputContainer">
+                    <form>
+                        <TextField required label="Course ID" id="courseID" variant="filled" sx={{ my: 1, width: '50%' }} />
+                        <TextField required fullWidth label="Name of the Class" id="name" sx={{ my: 1 }} variant="filled" />
+                        <TextField required fullWidth label="Course Description" id="description" multiline
+                            rows={4} sx={{ my: 1 }} variant="filled" />
+                        <TextField required type={'number'} label="Credits" id="credits" sx={{ my: 1, width: '50%' }}
+                            InputProps={{ inputProps: { min: 0, max: 10 } }} variant="filled" />
+                        <br></br>
+                        <Button variant="contained" startIcon={<InputIcon />} sx={{ left: '87%' }}>
+                            <input hidden type="submit" value="Submit" />
+                            Submit
+                        </Button>
+                    </form>
+                </div>
+            </div>
+        </>
+
     );
 
 }
--- src/navigation.js
+++ src/navigation.js
@@ -18,19 +18,21 @@ const LinksContainer = styled('div')({
 const Navigation = () => {
     return (
         < >
-            <header id="header" className="fixed-top">
-                <div className="container d-flex align-items-center justify-content-between">
-                    <h1 className="logo">Messiah University</h1>
-                    <NavigationContainer>
-                        <LinksContainer>
-                            <Link to="/home">4 Year Plan</Link>
-                            <Link to="">Prerequisites</Link>
-                            <Link to="">All Courses</Link>
-                            <Link to="/admin">Admin</Link>
-                        </LinksContainer>
-                    </NavigationContainer>
-                </div>
-            </header>
+            <div className="App">
+                <header id="header" className="fixed-top">
+                    <div className="main">
+                        <h1 className="logo">Messiah University</h1>
+                        <NavigationContainer>
+                            <LinksContainer>
+                                <Link to="/home">4 Year Plan</Link>
+                                <Link to="">Prerequisites</Link>
+                                <Link to="">All Courses</Link>
+                                <Link to="/admin">Admin</Link>
+                            </LinksContainer>
+                        </NavigationContainer>
+                    </div>
+                </header>
+            </div>
             <Outlet />
         </>
     );
