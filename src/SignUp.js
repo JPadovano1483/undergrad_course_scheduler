b3b561c43dbc78df1a10e3d265af043c6d9e054a
andrewcoldsmith <andrewcoldsmith@gmail.com>
2022-10-24 23:18:57 -0400

    admin and signup functionality

--- src/SignUp.js
+++ src/SignUp.js
@@ -1,19 +1,26 @@
 import * as React from 'react';
+import { useState } from 'react';
 import {Avatar, Button, TextField} from '@mui/material';
 import {Grid, Box,  Typography, Container} from '@mui/material';
 import {Link} from 'react-router-dom';
-
+import Axios from 'axios';
 
 
 export default function SignUp() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
-  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confPassword, setConfPassword] = useState("");
+
+  const addUser = () => {
+      console.log("Adding user.");
+      Axios.post(`http://localhost:3001/signup`, {
+          email: email,
+          password: password,
+          confPassword: confPassword,
+      }).then((response) => {
+          console.log(response);
+      });
+  }
 
   return (
     <Container component ="root"
@@ -36,7 +43,7 @@ export default function SignUp() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -67,6 +74,9 @@ export default function SignUp() {
                   label="Email Address"
                   name="email"
                   autoComplete="email"
+                  onChange={(e) => {
+                    setEmail(e.target.value)
+                  }} 
                 />
               </Grid>
               <Grid item xs={6}>
@@ -78,31 +88,37 @@ export default function SignUp() {
                   type="password"
                   id="password"
                   autoComplete="new-password"
+                  onChange={(e) => {
+                    setPassword(e.target.value)
+                  }} 
                 />
               </Grid>
               <Grid item xs={6}>
                 <TextField
                   required
                   fullWidth
-                  name="password"
+                  name="confPassword"
                   label="Confirm Password"
                   type="password"
-                  id="password"
+                  id="confPassword"
                   autoComplete="new-password"
+                  onChange={(e) => {
+                    setConfPassword(e.target.value)
+                  }} 
                 />
               </Grid>
               
             </Grid>
-            <Link to ="/home">
+            {/* <Link to ="/home"> */}
             <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                sx={{ mt: 3, mb: 2 }}
-                >
-                Sign Up
-              </Button>
-            </Link>
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              onClick={addUser}
+            >
+              Sign Up
+            </Button>
+            {/* </Link> */}
             <Grid container justifyContent="flex-end">
             <Link to ="/">
               <Grid item
