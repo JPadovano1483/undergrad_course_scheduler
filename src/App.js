7051e0d8897bf2643e1e22fdda3d65f11db678b0
Jamie Padovano <jp1483@messiah.edu>
2022-11-17 08:28:45 -0500

    restructure index->app

--- src/App.js
+++ src/App.js
@@ -1,148 +1,40 @@
-import './css/App.css';
-import * as React from 'react';
-import logo from './images/MessiahLogo.JPG';
-import { Avatar, Button, TextField, FormControlLabel } from '@mui/material';
-import { Checkbox, Grid, Box, Typography, Container, Alert} from '@mui/material';
-import { Link } from 'react-router-dom';
-import Axios from 'axios';
-import { useState } from 'react';
-// import { connect } from 'react-redux';
-//import { connect } from 'react-redux';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Home from './Home';
+import Admin from './Admin';
+import SignUp from './SignUp';
+import Account from "./Account";
+import ForgotPassword from './ForgotPassword';
+import EmailConfirm from './EmailConfirm';
+import Reset from './Reset';
 
-export default function SignIn() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-  };
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const login = () => {
-    Axios.post("http://localhost:3001/login", {
-      email: email,
-      password: password,
-    }).then((response) => {
-      if (response.data[0].username !== undefined) {
-        console.log(response.data[0].username);
-        console.log(response.data[0].password);
-        window.location.href = "http://localhost:3000/home";
-      }
-      else 
-      {
-        console.log(response.data)
-        let form = document.getElementById("alert_test");
-        form.style.visibility = 'visible';
-        //document.getElementById('dip').hidden = true
-         
-
-          
-      }
-    });
-  }
-
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+function App() {
   return (
-    <Container component="root"
-      sx={{
-        backgroundColor: '#002856',
-        color: 'black',
-      }}>
-      <Container component="main" maxWidth="xs"
-        sx={{
-          color: 'black',
-          mt: 5
-        }}>
-
-        {/* <CssBaseline /> */}
-
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-
-          <img src={logo} className="App-logo" alt="logo" />
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Alert component = 'alert_test' variant = "filled" severity ="error"
-            sx = {{
-              visibility:'hidden'
-            }}>
-              Warning! Email or password is incorrect
-            </Alert>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-              onClick={login}
-            >
-
-              Sign In
-            </Button>
-
-
-            <Grid container='test'>
-              <Link to="/ForgotPassword">
-                <Grid item xs
-                  sx={{ color: '#4007a2', mr: 4 }}
-                >
-
-                  Forgot password?
-
-                </Grid>
-              </Link>
-              <Link to="/SignUp">
-                <Grid item xs
-                  sx={{ color: '#4007a2' }}
-                >
-
-                  Don't have an account?
-                  Sign Up
-
-                </Grid>
-              </Link>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </Container>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='home' element={<Home />} />
+        <Route path='admin' element={<Admin />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='account' element={<Account />} />
+        <Route path='signup' element={<SignUp />} />
+        <Route path='forgotpassword' element={<ForgotPassword />} />
+        <Route path='emailconfirm' element={<EmailConfirm />} />
+        <Route path='reset' element={<Reset />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './Home';
-import Admin from './Admin';
-import SignUp from './SignUp';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import store from './store/store';
-import Account from "./Account";
-import ForgotPassword from './ForgotPassword';
-import EmailConfirm from './EmailConfirm';
-import Reset from './Reset';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Provider store={store}><App /></Provider>} />
-      <Route path='home' element={<Home />} />
-      <Route path='admin' element={<Admin />} />
-      <Route path='signup' element={<SignUp />} />
-      <Route path='account' element={<Account />} />
-      <Route path='signup' element={<SignUp />} />
-      <Route path='forgotpassword' element={<ForgotPassword />} />
-      <Route path='emailconfirm' element={<EmailConfirm />} />
-      <Route path='reset' element={<Reset />} />
-    </Routes>
-  </BrowserRouter>
+import App from "./App.js";  
+ 
+ReactDOM.render(   
+    <Provider store={store}><App /></Provider> ,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
