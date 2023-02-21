7348b506381816a31a0ff936237bb46ba37c5f87
andrewcoldsmith <andrewcoldsmith@gmail.com>
2023-02-20 23:16:49 -0500

    user specific data, admin page blocked, and prereq checking

--- server/server.js
+++ server/server.js
@@ -52,23 +52,30 @@ app.get("/allCourses", (req, res) => {
     });
 });
 
-app.get("/prereq", (req, res) => {
-  const course_id = req.body.id;
-  const sem = [req.body.sem1, req.body.sem2, req.body.sem3, req.body.sem4, req.body.sem5, req.body.sem6, req.body.sem7];
+app.post("/prereq", (req, res) => {
+  const semsterId = req.body.semesterId;
+  const semesters = req.body.semesters;
+  const courseId = req.body.courseId;
+  console.log("semsterId " + semsterId);
+  console.log("semesters " + semesters);
+  console.log("courseId " + courseId);
+  // const sem = [req.body.sem1, req.body.sem2, req.body.sem3, req.body.sem4, req.body.sem5, req.body.sem6, req.body.sem7];
   db.query("SELECT prerequisite_id FROM prerequisite WHERE course_id = ?",
-    [course_id],
+    [courseId],
     (err, result) => {
       if (err) {
         console.log(err);
       }
-      if (result.length > 0) {
-        for (let i = 0; i < result.length; i++) {
-          for (let j = 0; j < sem.length; j++) {
-            if (result[i] == sem[j]) {
-              res.send(false);
-            }
+      else if (result.length > 0) {
+        console.log("result.length " + result.length);
+        let satisfied = true;
+        for (let i = 0; i < semsterId - 2; i++) {
+          console.log("i " + i);
+          if (!result.some(prereqId => semesters[i].includes(prereqId))) {
+            satisfied = false;
           }
         }
+        res.send(satisfied);
       }
       else {
         console.log("No prerequisites.");
@@ -196,39 +203,26 @@ app.post("/account", (req, res) => {
     });
 });
 
-app.get("/profile",(req, res) => {
-  const first_name = req.body.first_name;
-  const last_name = req.body.last_name;
-  const username = req.body.username;
-  //const grade_level = req.body.grade_level;
+// app.get("/profile",(req, res) => {
+//   const first_name = req.body.first_name;
+//   const last_name = req.body.last_name;
+//   const username = req.body.username;
+//   //const grade_level = req.body.grade_level;
 
-  db.query("SELECT first_name, last_name, username, grade_level FROM user WHERE username = ?",
-  'jamie_padovano',
-  (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-    });
+//   db.query("SELECT first_name, last_name, username, grade_level FROM user WHERE username = ?",
+//   'jamie_padovano',
+//   (err, result) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       res.send(result);
+//     }
+//     });
   
-});
+// });
 
-app.get("/accountInfo",(req, res) => {
-  db.query("SELECT * FROM user WHERE username = ?",
-  'jamie_padovano',
-  (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-    });
-  
-});
-
-app.get("/plan/:id", (req, res) => {
-  const user_id = req.params.id;
+app.post("/plan/:id", (req, res) => {
+  const userId = req.params.id;
   const reqUser = req.body.reqUser;
   const targetUser = req.body.targetUser;
   const role = req.body.role;
@@ -236,23 +230,23 @@ app.get("/plan/:id", (req, res) => {
   let userValidation = userCtrlCheck(reqUser, targetUser, role);
 
   if (userValidation) {
-    db.query(`SELECT semester_id, course_id, course_name, credit_num FROM user JOIN semester using(user_id) JOIN user_course using(semester_id) JOIN course using(course_id) WHERE user.user_id=?`,
-      user_id,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      });
+  db.query(`SELECT semester_id, course_id, course_name, credit_num FROM user JOIN semester using(user_id) JOIN user_course using(semester_id) JOIN course using(course_id) WHERE user.user_id=?`,
+    [userId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
   }
   else {
     console.log("Not Authorized");
   }
 });
 
-app.get("/semester/:id", (req, res) => {
-  const semester_id = req.params.id;
+app.post("/semester/:id", (req, res) => {
+  const semesterId = req.params.id;
   const reqUser = req.body.reqUser;
   const targetUser = req.body.targetUser;
   const role = req.body.role;
@@ -260,40 +254,53 @@ app.get("/semester/:id", (req, res) => {
   let userValidation = userCtrlCheck(reqUser, targetUser, role);
 
   if (userValidation) {
-    db.query(`SELECT course_id, course_name, credit_num, semester_id FROM user JOIN semester using(user_id) JOIN user_course using(semester_id) JOIN course using(course_id) WHERE semester_id=?`,
-      semester_id,
-      (err, result) => {
-        if (err) {
-          console.log(err);
-        } else {
-          res.send(result);
-        }
-      });
+    db.query("SELECT course_id, course_name, credit_num, semester_id, user.user_id FROM course JOIN user_course using(course_id) JOIN semester using(semester_id) INNER JOIN user ON user_course.user_id=user.user_id WHERE semester_id=? AND user.user_id=?",
+    [semesterId, reqUser],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
   }
   else {
     console.log("Not Authorized");
   }
+});
+
+app.post("/semCount", (req, res) => {
+  const userId = req.body.userId;
+  db.query(`SELECT semester_num FROM semester WHERE user_id=? ORDER BY semester_num DESC LIMIT 1`,
+    [userId],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    });
 });
 
 app.post("/reset", (req, res) => {
   const password = req.body.password;
   const confPassword = req.body.confPassword;
   const email = req.body.email;
-    if (password == confPassword) {
-      db.query(`UPDATE user SET password = ? WHERE username = ?`,
-        [password, email],
-        (err, result) => {
-          if (err) {
-            console.log(err);
-          } else {
-            console.log("Password changed.");
-            res.send(result);
-          }
-        });
-    }
-    else {
-      console.log("Passwords do not match.");
-      res.send("Passwords do not match.");
+  if (password == confPassword) {
+    db.query(`UPDATE user SET password = ? WHERE username = ?`,
+      [password, email],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Password changed.");
+          res.send(result);
+        }
+      });
+  }
+  else {
+    console.log("Passwords do not match.");
+    res.send("Passwords do not match.");
   }
 });
 
@@ -325,7 +332,38 @@ app.post("/deleteCourse", (req, res) => {
       }
     }
   )
-})
+});
+
+app.post("/addSemester", (req, res) => {
+  const user_id = req.body.user_id;
+  const semester_num = req.body.semester_num;
+  db.query(`INSERT INTO semester (user_id, semester_num) VALUES (?,?)`,
+    [user_id, semester_num],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  )
+});
+
+app.post("/deleteSemester", (req, res) => {
+  const user_id = req.body.user_id;
+  const semester_num = req.body.semester_num;
+  // db.query(`DELETE FROM semester WHERE user_id=? ORDER BY semester_num DESC LIMIT 1`,
+  db.query(`DELETE FROM semester WHERE user_id=? AND semester_num=?`,
+    [user_id, semester_num],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  )
+});
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/../src/build/index.html'));
@@ -336,3 +374,7 @@ const PORT = 3001;
 app.listen(process.env.PORT || PORT, () => {
   console.log(`Your server is running on port ${PORT}`);
 });
+
+setInterval(function () {
+  db.query('SELECT 1');
+}, 5000);
