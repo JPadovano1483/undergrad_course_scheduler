5c553b4ffbeceb7303b9569f013696902de67098
Sebastian Alejos Acosta <98053292+bastialejos@users.noreply.github.com>
2023-04-23 22:36:57 -0400

    test 1

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -37,6 +37,7 @@ root.render(
     </Routes>
   </BrowserRouter>
 );
+console.log("hello there");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
