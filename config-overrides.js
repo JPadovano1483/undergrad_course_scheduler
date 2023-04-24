dc0163c954dbd1a59483df3b645be61592771d78
Sebastian Alejos Acosta <98053292+bastialejos@users.noreply.github.com>
2023-04-23 22:52:22 -0400

    test 3

--- config-overrides.js
+++ config-overrides.js
@@ -4,8 +4,8 @@ module.exports = {
         // Changing public to static
         paths.appPublic = path.resolve(__dirname, 'react-client/public');
         paths.appHtml = path.resolve(__dirname, 'react-client/public/index.html');
-        paths.appIndexJs = path.resolve(__dirname, 'react-client/src/index.js');
         paths.appSrc = path.resolve(__dirname, 'react-client/src');
+        paths.appIndexJs = path.resolve(__dirname, 'react-client/src/index.js');
         return paths;
     }
 }
